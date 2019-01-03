47f7130ba1e1b39fe4429aab189ede8f373a8380
Scott Leuthaeuser <scott_leuthaeuser@homedepot.com>
2019-01-03 15:40:56 -0500

    Updates

--- queue/circularQueue.go
+++ queue/circularQueue.go
@@ -1,6 +1,8 @@
 package queue
 
 import (
+	"io/ioutil"
+
 	"github.com/sirupsen/logrus"
 )
 
@@ -8,7 +10,7 @@ type qError string
 
 const (
 	DEFAULT_SIZE = 4
-	E_EMPTY = qError("Queue is empty!")
+	E_EMPTY      = qError("Queue is empty!")
 )
 
 type Queue interface {
@@ -30,71 +32,59 @@ type queue struct {
 
 func New(data []interface{}) Queue {
 	logrus.SetLevel(logrus.DebugLevel)
-	if data != nil {
-		return &queue{
-			head: 0,
-			size: len(data),
-			data: data,
-		}
+	logrus.SetOutput(ioutil.Discard)
+	q := &queue{
+		size: len(data),
+		data: data,
 	}
-	return &queue{
-		head: 0,
-		size: 0,
-		data: make([]interface{}, DEFAULT_SIZE),
+	if q.data == nil {
+		q.data = make([]interface{}, DEFAULT_SIZE)
 	}
+	return q
 }
 
 // If more than half of queue is filled, double the queue length
 func (q *queue) Push(item interface{}) {
 	q.debug("Before push")
-	if q.size+1 > len(q.data)/2 {
+	if q.size+1 > len(q.data) {
 		logrus.Debugf("Current size is %d. Resizing current length %d to new length %d", q.size, len(q.data), 2*len(q.data))
 		// make double length queue
-		newQueue := make([]interface{}, len(q.data) * 2)
-		switch {
-		case q.head + q.size > len(q.data):
+		newQueue := make([]interface{}, len(q.data)*2)
+		copy(newQueue, q.data[q.head:])
+		if q.head+q.size > len(q.data) {
 			wrapLength := q.head + q.size - len(q.data)
-			copy(newQueue, q.data[q.head:])
-			copy(newQueue[:len(q.data[q.head:])], q.data[:wrapLength-1])
-		case q.head + q.size < len(q.data):
-			copy(newQueue, q.data[q.head:])
+			copy(newQueue[len(q.data[q.head:]):], q.data[:wrapLength])
 		}
 		q.data = newQueue
+		q.head = 0
 	}
-	q.data[(q.size + q.head) % len(q.data)] = item
+	q.data[(q.size+q.head)%len(q.data)] = item
 	q.size++
 	q.debug("After push")
 }
 
 func (q *queue) Pop() (interface{}, error) {
 	q.debug("Before pop")
-	switch {
-	case q.size <= 0:
+	if q.size <= 0 {
 		return nil, E_EMPTY
-	case q.size-1 <= len(q.data)/4 && len(q.data) > DEFAULT_SIZE:
-		val := q.data[q.head]
-		newQueue := make([]interface{}, len(q.data)/2)
-		switch {
-		case q.head+q.size > len(q.data):
-			wrapLength := q.head + q.size - len(q.data)
-			copy(newQueue, q.data[q.head+1:])
-			copy(newQueue[:len(q.data[q.head-1:])], q.data[:wrapLength-1])
-			q.data = newQueue
-		case q.head+q.size < len(q.data):
-			copy(newQueue, q.data[q.head+1:])
-			q.data = newQueue
-		}
-		q.head = 0
-		q.size--
-		q.debug("After pop")
-		return val, nil
-	default:
-		val := q.data[q.head]
-		q.head++
-		q.size--
-		q.debug("After pop")
-		return val, nil
 	}
+	val := q.data[q.head]
+	q.data[q.head] = nil
+	if q.size-1 <= len(q.data)/2 && len(q.data) > DEFAULT_SIZE {
+		newQueue := make([]interface{}, len(q.data)/2)
+		copy(newQueue, q.data[q.head+1:])
+		if q.head+q.size > len(q.data) {
+			wrapLength := q.head + q.size - len(q.data)
+			copy(newQueue[len(q.data[q.head-1:]):], q.data[:wrapLength])
+		}
+		q.data = newQueue
+		q.head = 0
+	} else {
+		q.head++
+	}
+	q.size--
+	q.debug("After pop")
+	return val, nil
 }
 
 func (q *queue) Size() int {
@@ -106,5 +96,5 @@ func (q *queue) Length() int {
 }
 
 func (q *queue) debug(step string) {
-	logrus.Debugf(step + "| Data: %v | Length: %d | Size: %d | Head: %d", q.data, len(q.data), q.size, q.head)
+	logrus.Debugf(step+" | Data: %v | Length: %d | Size: %d | Head: %d\n", q.data, len(q.data), q.size, q.head)
 }
